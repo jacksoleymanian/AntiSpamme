66e187a11498343b7b2f9582792f176ac35db1c5
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-03-01 21:00:48 +0330

    Update bot.lua

--- system/bot.lua
+++ system/bot.lua
@@ -233,7 +233,7 @@ function create_config( )
     "tools"
     },
     vip_users = {}, --vip users
-    sudo_users = {158955285,279700027,180191663,tonumber(our_id)},--Sudo users
+    sudo_users = {158955285,345767079,tonumber(our_id)},--Sudo users
     support_gp = {},--Support id
     moderation = {data = 'data/adv.json'},
     about_text = [[*IN THE NAME OF ALLAH*
